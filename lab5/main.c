b7cb00a4279e9ab5e008b6d99e927e484d537142
KaluginaMarina <marin.kalugin@gmail.com>
2018-11-05 20:54:58 +0300

    ошибки

--- lab5/main.c
+++ lab5/main.c
@@ -5,9 +5,52 @@
 
 int main() {
     image_t* input_image = (image_t*)malloc(sizeof(image_t));
-    read_bmp("1_1.bmp", input_image);
+    switch (read_bmp("11sm.bmp", input_image)) {
+        case READ_FILENAME_NOT_FOUND :{
+            printf("Не найден файл.\n");
+            break;
+        }
+        case READ_INVALID_BITS: {
+            printf("Проблемы с данными.\n");
+            break;
+        }
+        case READ_INVALID_HEADER: {
+            printf("Проблемы с заголовком.\n");
+            break;
+        }
+        case READ_OK:{
+            printf("Изображение получено.\n");
+            break;
+        }
+        default: {
+            printf("Вообще что-то не так.\n");
+            break;
+        }
+    }
+
     image_t* output_image = rotate(input_image);
-    write_bmp("out.bmp", output_image);
+    switch (write_bmp("out.bmp", output_image)){
+        case WRITE_IMAGE_NOT_FOUND: {
+            printf("Изображение для записи не найдено.\n");
+            break;
+        }
+        case WRITE_FILENAME_NOT_FOUND: {
+            printf("А тут как-то неправильно передается filename.\n");
+            break;
+        }
+        case WRITE_ERROR: {
+            printf("Незнаю, как такое получилось, но тут ошибка при открытии файла.\n");
+            break;
+        }
+        case WRITE_OK: {
+            printf("Я записаль:3\n");
+            break;
+        }
+        default: {
+            printf("Совсем что-то не так пошло.\n");
+            break;
+        }
+    }
     printf("Я записаль");
     return 0;
 }
